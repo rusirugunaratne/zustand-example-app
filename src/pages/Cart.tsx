9455af17e033eb1aaa7ef4aea67707440909767d
rusirugunaratne <info.rusiru@gmail.com>
2025-03-16 18:10:51 +0530

    cart styling added

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,4 +1,4 @@
-import { Grid2, Paper, Typography } from "@mui/material";
+import { Grid2, Typography } from "@mui/material";
 import { items } from "../data/items";
 
 const Cart = () => {
@@ -6,9 +6,9 @@ const Cart = () => {
     <Grid2 container spacing={2} padding={2}>
       {items.map((item) => (
         <Grid2 size={4} key={item.id}>
-          <Paper elevation={3} sx={{ padding: 2 }}>
-            <Typography variant='h6'>{item.name}</Typography>
-          </Paper>
+          <Typography sx={{ margin: 2 }} variant='h6'>
+            {item.name}
+          </Typography>
         </Grid2>
       ))}
     </Grid2>
